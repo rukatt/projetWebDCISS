96f988dde47ca701c88b192447bebf553893f1d4
Rudy <kattouche.rudy@gmail.com>
2016-03-15 10:50:17 +0100

    new commit

--- js/secretary.js
+++ js/secretary.js
@@ -1,1 +1,8 @@
 require( "./secretary.css" );
+
+(function(){
+    var utils = require( "./utils.js" );
+    var xhr = utils.XHR("GET", "data/cabinetInfirmier.xml").then( function(data) {console.log(data);} )
+
+    var parser = new DOMParser();
+}());
